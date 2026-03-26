45ef4856a861e716bb4dc4e76608645fba4e392b
Sofia Quintero <slquinte@syr.edu>
2026-03-26 19:23:31 +0000

    file name fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ lab2_page = st.Page('Lab2.py', title = "Lab 2")
 lab3_page = st.Page('Lab3.py', title = "Lab 3")
 lab4_page = st.Page('Lab4.py', title = "Lab 4")
 lab5_page = st.Page('Lab5.py', title = "Lab 5")
-lab6_page1 = st.Page('Lab6-openai_responses.py', title = "Lab 6- OpenAI Responses")
+lab6_page1 = st.Page('Lab6-responses.py', title = "Lab 6- OpenAI Responses")
 lab6_page2 = st.Page('Lab6-langchain.py', title = "Lab 6- LangChain")
 
 pg = st.navigation([lab6_page2, lab6_page1, lab5_page, lab4_page, 
