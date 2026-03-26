3fd348dfc563c7b9796eda7c42937cf6cb4e8ca7
Sofia Quintero <slquinte@syr.edu>
2026-03-26 19:25:28 +0000

    file name fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ lab2_page = st.Page('Lab2.py', title = "Lab 2")
 lab3_page = st.Page('Lab3.py', title = "Lab 3")
 lab4_page = st.Page('Lab4.py', title = "Lab 4")
 lab5_page = st.Page('Lab5.py', title = "Lab 5")
-lab6_page1 = st.Page('Lab6-responses.py', title = "Lab 6- OpenAI Responses")
-lab6_page2 = st.Page('Lab6-langchain.py', title = "Lab 6- LangChain")
+responses_page = st.Page('Lab6-responses.py', title = "Lab 6- OpenAI Responses")
+langchain_page = st.Page('Lab6-langchain.py', title = "Lab 6- LangChain")
 
-pg = st.navigation([lab6_page2, lab6_page1, lab5_page, lab4_page, 
+pg = st.navigation([langchain_page, responses_page, lab5_page, lab4_page, 
                     lab3_page, lab2_page, lab1_page]) # default to lab 6.2
 st.set_page_config(page_title = 'IST 488 Labs')
 pg.run()
