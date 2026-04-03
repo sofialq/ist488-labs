6c8f645df64e10a30763f6ffa42d5929408df04f
Sofia Quintero <slquinte@syr.edu>
2026-04-03 18:03:15 -0400

    web search tool added

--- Lab7-LocalModel.py
+++ Lab7-LocalModel.py
@@ -1,5 +1,35 @@
+import ollama
 import streamlit as st
 from ollama import chat
+
+# define tool schema 
+import ollama
+
+# Step 1: Define the tool schema (like Lab 5's get_current_weather tool)
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "web_search",
+            "description": "Search the web for current information based on the user's question",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "query": {
+                        "type": "string",
+                        "description": "The search query to look up"
+                    }
+                },
+                "required": ["query"]
+            }
+        }
+    }
+]
+
+# create client
+client = ollama.Client(
+    api_key=st.secrets["OLLAMA_API_KEY"]
+)
 
 # initialize session state
 if 'messages' not in st.session_state:
@@ -14,7 +44,23 @@ if prompt := st.chat_input("Ask a question"):
     st.write(f"you said: {prompt}")
     
     try:
-        stream = chat(
+        no_web = client.chat(
+            model='mistral:latest',
+            messages=[{"role": "user", "content": prompt}],
+            tools=tools
+        )
+
+        messages=[{"role": "user", "content": prompt}]
+
+        if no_web.message.tool_calls:
+            for tool_call in no_web.message.tool_calls:
+                query = tool_call.function.arguments['query']
+
+                web_search = ollama.web_search(prompt)
+
+                messages.append({"role": "tool", "content": str(web_search)})
+
+        stream = client.chat(
             model='mistral:latest',
             messages=[{"role": "user", "content": prompt}],
             stream=True,
